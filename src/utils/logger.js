68c10b3933aa9c7ce8e4218e9fd78c6dbc402850
Ryan Smith <0ryansmith1994@gmail.com>
2015-07-03 15:10:30 +0100

    Wraps response logging.

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -7,20 +7,25 @@ module.exports = function (requestCfg, res) {
     console.log("PARTS = ", requestCfg.multipart);
     console.log("BODY = ", requestCfg.body);
     console.log("::::::");
-    if (res.statusCode === 500) {
-        console.log("RES-MESSAGE = ", res.body.split("<p class=\"exc-message\">")[1].split("</p>")[0]);
-        console.log("RES-TRACE = ", res.body.split("<div class=\"frame-code-container \">")[1].split("<div class=\"details\">")[0]);
-    } else if (res.statusCode === 400) {
-        json = JSON.parse(res.body);
-        console.log("RES-MESSAGE = ", json.message);
-        console.log("RES-TRACE = ", Array.isArray(json.trace) ? json.trace.map(function (item) {
-            var file = String(item.file);
-            var split = (file.indexOf("/") !== -1) ? "/" : "\\";
-            return "" + file.split(split).slice(3).join("/") + " - " + item.line;
-        }) : json.trace);
-    } else {
-        console.log("RES = ", res.statusCode, res.body);
-        console.log("ETAG = ", res.headers.etag);
+    console.log('RES-JS-TYPE', typeof res);
+    try {
+        if (res.statusCode === 500) {
+            console.log("RES-MESSAGE = ", res.body.split("<p class=\"exc-message\">")[1].split("</p>")[0]);
+            console.log("RES-TRACE = ", res.body.split("<div class=\"frame-code-container \">")[1].split("<div class=\"details\">")[0]);
+        } else if (res.statusCode === 400) {
+            json = JSON.parse(res.body);
+            console.log("RES-MESSAGE = ", json.message);
+            console.log("RES-TRACE = ", Array.isArray(json.trace) ? json.trace.map(function (item) {
+                var file = String(item.file);
+                var split = (file.indexOf("/") !== -1) ? "/" : "\\";
+                return "" + file.split(split).slice(3).join("/") + " - " + item.line;
+            }) : json.trace);
+        } else {
+            console.log("RES = ", res.statusCode, res.body);
+            console.log("ETAG = ", res.headers.etag);
+        }
+    } catch (ex) {
+        console.log(ex.message);
     }
     console.log("------");
 };
